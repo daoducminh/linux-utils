ab33701ae20d39a7e704b6f2c65084b621c290ec
Minh Dao <daoducminh1997@gmail.com>
2023-08-09 07:12:16 +0000

    Update

--- switch_g_shell.sh
+++ switch_g_shell.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+declare -A accounts=(
+    [1]="minhdd@bravestars.com"
+    [2]="20156051@student.hust.edu.vn"
+)
+
+declare -A host_names=(
+    [1]="minhdd"
+    [2]="20156051"
+)
+
 # Get the gcloud account from the first argument, use the default value if not provided
-gcloud_account=${1:-20156051@student.hust.edu.vn}
+choice=${1:-1}
+gcloud_account=${accounts[$choice]}
+host_name=${host_names[$choice]}
 
 # Get the output of the gcloud command
 gcloud_output=$(gcloud cloud-shell ssh --authorize-session --force-key-file-overwrite --dry-run --account="$gcloud_account" 2>&1)
@@ -11,25 +23,25 @@ public_ip=$(echo "$gcloud_output" | awk -F'@| -- ' '{print $2}')
 user=$(echo "$gcloud_output" | awk -F'@| -- ' '{print $1}' | awk '{print $NF}')
 
 # Check if host g_shell exists in ~/.ssh/config
-if grep -q "Host g_shell" ~/.ssh/config; then
+if grep -q "Host $host_name" ~/.ssh/config; then
     # Check if the public IP address and user are not empty
     if [ -n "$public_ip" ] && [ -n "$user" ]; then
-        # Replace the HostName and User for the g_shell host in ~/.ssh/config with the new IP address and user
-        # sed -i "/^Host g_shell$/!b; {n; s/^    HostName .*/    HostName $public_ip/}" ~/.ssh/config
-        # sed -i "/^Host g_shell$/!b; {n; s/^    User .*/    User $user/}" ~/.ssh/config
-        sed -i -E "/^Host g_shell$/ { n; s/^\s*HostName\s+[^\s]+$/    HostName $public_ip/; n; s/^\s*User\s+[^\s]+$/    User $user/; }" ~/.ssh/config
-        echo "Account $gcloud_account updated HostName and User for 'g_shell' with new IP address: $public_ip and user: $user"
+        # Replace the HostName and User for the $host_name host in ~/.ssh/config with the new IP address and user
+        #sed -i "/^Host $host_name$/!b; {n; s/^    HostName .*/    HostName $public_ip/}" ~/.ssh/config
+        #sed -i "/^Host $host_name$/!b; {n; s/^    User .*/    User $user/}" ~/.ssh/config
+        sed -i -E "/^Host "$host_name"$/ { n; s/^\s*HostName\s+[^\s]+$/    HostName $public_ip/; n; s/^\s*User\s+[^\s]+$/    User $user/; }" ~/.ssh/config
+        echo "Account $gcloud_account updated HostName and User for '$host_name' with new IP address: $public_ip and user: $user"
     else
         echo "Failed to retrieve public IP address or user."
     fi
 else
     # Append the new host profile to ~/.ssh/config
-    echo "Host g_shell
+    echo "Host $host_name
     HostName $public_ip
     User $user
+    IdentityFile ~/.ssh/$host_name
     Port 6000
-    StrictHostKeyChecking no
-    IdentityFile ~/.ssh/google_compute_engine" >> ~/.ssh/config
+    StrictHostKeyChecking no" >>~/.ssh/config
 
-    echo "Host profile g_shell created and added to ~/.ssh/config."
+    echo "Host profile $host_name created and added to ~/.ssh/config."
 fi
