9a3708c4e640d20a8aabaf835483dcad0a13910e
Minh Dao <daoducminh1997@gmail.com>
2025-03-12 15:11:08 +0700

    Update

--- update_neovim.sh
+++ update_neovim.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # Download neovim tarball
-curl -sfL -C - -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+curl -sfL -C - -o nvim.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
 
 # Extract tarball
 tar -xf nvim.tar.gz
 
 # Update icon in desktop file
-sed -i 's/Icon=nvim/Icon=\/opt\/nvim\/share\/icons\/hicolor\/128x128\/apps\/nvim.png/g' nvim-linux64/share/applications/nvim.desktop
+sed -i 's/Icon=nvim/Icon=\/opt\/nvim\/share\/icons\/hicolor\/128x128\/apps\/nvim.png/g' nvim-linux-x86_64/share/applications/nvim.desktop
 
 # Copy and replace
-sudo cp -rf nvim-linux64/* /opt/nvim/
+sudo cp -rf nvim-linux-x86_64/* /opt/nvim/
 
 # Clean up
 rm nvim.tar.gz
-rm -rf nvim-linux64
+rm -rf nvim-linux-x86_64
